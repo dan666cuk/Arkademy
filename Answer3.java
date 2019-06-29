202d0dabc44de1e19f901c8414985e42df6115fc
dan666cuk <rtanjung38@yahoo.com>
2019-06-29 21:21:30 +0700

    Create Answer3.java

---
+++ Answer3.java
@@ -0,0 +1,34 @@
+package arkademy;
+
+/**
+ *
+ * @author Rezki Tanjung
+ */
+public class Answer3 {
+    public static void main(String[] args) {
+        int n = 11; 
+	printPattern(n); 
+    }
+    static void printPattern(int n){ 
+	int i, j; 
+	for (i = 1; i <= n; i++) { 
+	// Print spaces 
+            for (j = 1; j < i; j++) { 
+		System.out.print(" "); 
+            } 
+	
+        // Print hollow inverted pyramid 
+            for (j = 1; j <= (n * 2 - (2 * i - 1));j++) { 
+		if (i == 1 || j == 1 || j == (n * 2 - (2 * i - 1))) { 
+                    System.out.print("*"); 
+                } else { 
+                    System.out.print(" "); 
+		} 
+            } 
+	
+        // next line 
+	System.out.println(); 
+	} 
+    } 
+    
+}
